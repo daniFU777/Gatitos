2b61ae7a8ce1a233f76df93cc7300d43f592d4af
daniFU777 <danielprooooogamer41@gmail.com>
2024-11-20 12:21:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let isGenerating = false;  // Controla el estado de la generación de la imagen
 
 function generateCatImage() {
-    const button = document.querySelector("button");  // Seleccionamos el botón de generar
+    const button = document.getElementById('generateButton');  // Seleccionamos el botón de generar
     if (isGenerating) {
         return;  // Si ya se está generando una imagen, no hacer nada
     }
 
     isGenerating = true;  // Marcar que se está generando
+    button.style.display = 'none';  // Ocultar el botón
     button.disabled = true;  // Deshabilitar el botón
 
     const loader = document.getElementById('loader');
@@ -26,9 +27,10 @@ function generateCatImage() {
         loader.style.display = 'none'; // Ocultar el indicador de carga
         catImage.style.display = 'block'; // Mostrar la imagen
 
-        // Habilitar el botón después de 3 segundos
+        // Habilitar y mostrar el botón después de 3 segundos
         setTimeout(() => {
             isGenerating = false;  // Marcar como no generando
+            button.style.display = 'block';  // Mostrar el botón
             button.disabled = false;  // Habilitar el botón
         }, 3000);  // Esperar 3 segundos antes de permitir otro clic
     };
