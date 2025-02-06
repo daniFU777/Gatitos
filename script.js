fad557886371b628c86ed1ab20e29c28f7ca1c2e
daniFU777 <danielprooooogamer41@gmail.com>
2025-02-06 17:48:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ document.addEventListener("DOMContentLoaded", function() {
         fetch("https://api.thecatapi.com/v1/images/search")
             .then(response => response.json())
             .then(data => {
-                if (data.length > 0) {
+                if (data.length > 0 && data[0].url) {
                     catImage.src = data[0].url;
                     catImage.style.display = "block";
+                } else {
+                    console.error("No se recibieron datos válidos.");
                 }
             })
             .catch(error => console.error("Error al obtener la imagen:", error));
